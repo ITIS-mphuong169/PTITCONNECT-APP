e2dc01bcddadc9e5896b5c186caf3b1dd5a967d9
huyendtn <163633657+huyendtn24@users.noreply.github.com>
2026-04-15 13:13:12 +0700

    Update urls.py

--- backend/documents/urls.py
+++ backend/documents/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-
-from .views import document_detail_api, documents_api, document_subjects_api
+from .views import (
+    documents_api,
+    document_detail_api,
+    document_subjects_api,
+    document_view_api,  # 👈 thêm
+)
 
 urlpatterns = [
-    path("", documents_api, name="documents-api"),
-    path("subjects/", document_subjects_api, name="document-subjects"),
-    path("<int:pk>/", document_detail_api, name="document-detail"),
+    path("", documents_api),
+    path("subjects/", document_subjects_api),
+    path("<int:pk>/", document_detail_api),
+
+    path("<int:pk>/view/", document_view_api),
 ]
